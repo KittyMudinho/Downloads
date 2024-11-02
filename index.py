5281abb2f344db2aec4bcca45f544534f42ec7a7
Gustavo Sampaio <cientistadacompgustavosampaio@gmail.com>
2024-11-02 13:18:56 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,9 +3,9 @@ import json
 import requests
 import openpyxl
 import plotly.express as px
-#cidade=input('Diga a cidade que deseja saber a temperatura (Say the city you wish to know the temperature): ')
-key='9a40fecccf7d49d1935151140240211'
-request=json.loads(requests.get(f'http://api.weatherapi.com/v1/current.json?key={key}&q=Santos&aqi=no').content.decode('utf-8'))
+cidade=input('Diga a cidade que deseja saber a temperatura (Say the city you wish to know the temperature): ')
+key='YOUR_KEY'
+request=json.loads(requests.get(f'http://api.weatherapi.com/v1/current.json?key={key}&q={cidade}&aqi=no').content.decode('utf-8'))
 time=request['location']['localtime']
 temp=float(request['current']['temp_c'])
 print(f"Temperatura:{temp}°C\nData e hora:{time}")
